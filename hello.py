a04e2b614079df08d697a17ae3d2ae2c7026564e
galeb1 <galeb12243@gmail.com>
2022-05-06 13:18:23 +0900

    Task2
    5의 배수일 때 dream 나머지는 숫자 출력

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
 print('hello')
+# Task1
 for i in range(1, 11):
     if i % 3 == 0:
         print('year')
+    else:
+        print(i)
+# Task2
+for i in range(1, 21):
+    if i % 5 == 0:
+        print('dream')
     else:
         print(i)
